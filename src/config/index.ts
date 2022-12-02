7efd03ca14d322f72f0e54eddd7d941918e9b2a0
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-12-03 00:00:40 +0530

    feat: add mongodb integration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,30 @@
 import { config } from "dotenv-safe";
 
+export interface MongoConfig {
+    uri: string
+}
 export interface Config {
     env: string,
     apiPort: number | string,
-    port: number | string
+    port: number | string,
+    mongo: MongoConfig
 }
 
 config();
 const env = process.env.NODE_ENV || 'development';
 const apiPort = process.env.APIPORT || 5000;
 const port = process.env.PORT || 5000;
+const mongo: MongoConfig = {
+    uri: process.env.MONGO_URI
+}
 
 const appConfig: Config = {
     env,
     apiPort,
-    port
+    port,
+    mongo
 }
+
 export {
     appConfig
 };
