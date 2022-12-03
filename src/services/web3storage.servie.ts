60a68e07928176b2377c8f55a8d9826b259cdbcd
JithinKS <jithinks112@gmail.com>
2022-12-03 16:36:54 +0530

    added web3 storage service

---
+++ src/services/web3storage.servie.ts
@@ -0,0 +1,17 @@
+
+//@ts-ignore
+import { Web3Storage, getFilesFromPath } from 'web3.storage';
+
+export default class Web3StorageService {
+    storage: any;
+    constructor() {
+        this.storage = new Web3Storage({ token: process.env.WEB3_TOKEN })
+    }
+
+    async put(path:string) {
+        const file = await getFilesFromPath(path)
+        console.log(file)
+        const cid = await this.storage.put(file)
+        return cid;
+    }
+}
